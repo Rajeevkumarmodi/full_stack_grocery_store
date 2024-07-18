8ec4eef33cf2c6bd78f9ff490564a1a5a7a0ff48
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2024-07-18 11:10:07 +0530

    project frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage/Home";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
